878cb4ac44dce4243b5b50336866122ae007b085
ornao <reynoldsorna@gmail.com>
2023-02-25 16:07:11 +0000

    wire up APIs and confirm access to google sheets

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Below code taken from Code Institute's Love Sandwiches Walkthrough Project:
+# Getting Set Up (Creating the Google Sheets API)
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('bullshit')
+
+username = SHEET.worksheet('username')
+data = username.get_all_values()
+print(data)
+
+
 print("██████╗░██╗░░░██╗██╗░░░░░██╗░░░░░░██████╗██╗░░██╗██╗████████╗░░░░░░░░░███ ")
 print("██╔══██╗██║░░░██║██║░░░░░██║░░░░░██╔════╝██║░░██║██║╚══██╔══╝░░░░░░░░░███ ")
 print("██████╦╝██║░░░██║██║░░░░░██║░░░░░╚█████╗░███████║██║░░░██║░░░░░░░░░░░░███  ")
